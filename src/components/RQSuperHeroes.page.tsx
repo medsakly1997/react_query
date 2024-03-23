244a11d74c47a3b046da2b6d5ca1194fc6d04453
sakly mohamed khaled <saklymohamedkhaled@gmail.com>
2024-03-23 13:50:37 +0100

    feat(pages): add useQuery logic

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -1,7 +1,25 @@
+import axios from "axios";
 import React from "react";
-
+import { useQuery } from "react-query";
 const RQSuperHeroes = () => {
-  return <div>RQSuperHeroes</div>;
+  const fetchSuperComments = () => {
+    return axios.get("http://localhost:5000/superheroes");
+  };
+  const { data, isLoading, isError } = useQuery("comments", fetchSuperComments);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  console.log(data);
+  return (
+    <div>
+      <h2>Super Heroes</h2>
+      {data?.data.map((comment: any) => {
+        return <div key={comment.id}>{comment.text}</div>;
+      })}
+    </div>
+  );
 };
 
 export default RQSuperHeroes;
